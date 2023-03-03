15a3bc905090f3d0ce11131123532db1c70b3ca2
Valeria Krasnykh <valerie-kras@mail.ru>
2023-03-03 20:46:42 +0300

    right part added

--- gulpfile.js
+++ gulpfile.js
@@ -30,5 +30,10 @@ const destImages = () => {
   return src("src/images/**/*").pipe(dest("build/images"));
 };
 
+const destBootstrapJs = () => {
+  return src('node_modules/bootstrap/dist/js/bootstrap.min.js')
+    .pipe(dest('build/js/'));
+};
+
 exports.server = browserSyncJob;
-exports.build = parallel(buildSass, buildPug, destImages);
+exports.build = parallel(buildSass, buildPug, destImages, destBootstrapJs);
